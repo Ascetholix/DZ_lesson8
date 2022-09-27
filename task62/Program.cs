8688cfbf01668fe5d9460b3b7e084543075c3d08
Sangak <96950842+Ascetholix@users.noreply.github.com>
2022-09-28 00:16:47 +0300

    done all task

--- task62/Program.cs
+++ task62/Program.cs
@@ -6,54 +6,68 @@
 // 10 09 08 07
 Console.Clear();
 
-void PrintArrya(int[,] arg)   // Метод вывода матрицы
+int[,] FillArray(int[,] arr)
 {
-    for (int i = 0; i < arg.GetLength(0); i++) // rows - строки
+    int numbersInAnArray = arr.GetLength(0) * arr.GetLength(1); // числа в массиве
+    int i = 0;
+    int j = 0;
+    int side = 0;  // сторона
+    int circle = 0; // круг
+    arr[i, j] = 1;  // первый элемент масива = 1
+    for (int h = 2; h <= numbersInAnArray; h++) // цикл со второго элемента
     {
-        for (int j = 0; j < arg.GetLength(1); j++)  // columns - столбцы
+        if ((j + 1 < arr.GetLength(1) - circle && side == 0))
         {
-            Console.Write($" {arg[i, j]} ");
+            arr[i, j + 1] = h;
+            j++;
+            if (j == arr.GetLength(1) - 1 - circle) side++;
+        }
+        else if (i + 1 + circle < arr.GetLength(0) && side == 1)
+        {
+            arr[i + 1, j] = h;
+            i++;
+            if (i == arr.GetLength(0) - 1 - circle) side++;
+        }
+        else if (j - 1 - circle >= 0 && side == 2)
+        {
+            arr[i, j - 1] = h;
+            j--;
+            if (j == 0 + circle)
+            {
+                side++;
+                circle++;
+            }
+        }
+        else if (i - 1 - circle >= 0 && side == 3)
+        {
+            arr[i - 1, j] = h;
+            i--;
+            if (i == 0 + circle) side = 0;
+        }
+    }
+    return arr;
+}
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            string a = Convert.ToString((arr[i, j]));
+            if (1 == a.Length) Console.Write($"0{arr[i, j]} ");
+            else Console.Write($"{arr[i, j]} ");
         }
         Console.WriteLine();
     }
 }
 
-void FillArrya(int[,] arg)
-{
-    int[] mas = new int[arg.GetLength(0) * arg.GetLength(1)];
-    int cout = 0;
-    for (int i = 0; i < arg.GetLength(0); i++)
-    {
-        // bool ch = true;
 
-        for (int j = 0; j < arg.GetLength(1); j++)
-        {
+Console.Write("Введите количество строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
 
-            arg[i, j] = mas[cout];
-            cout++;
-            for (int k = 0; k < mas.Length; k++)
-            {
+int column = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[rows, column];
 
-                mas[cout] = k + 1;
-                break;
-
-            }
-
-        }
-    }
-}
-
-Console.Write("Введите длину строки ");
-int row = Convert.ToInt32(Console.ReadLine());
-
-Console.Write("Введите длину столбца ");
-int colums = Convert.ToInt32(Console.ReadLine());
-
-int[,] arr = new int[row, colums];
-
-FillArrya(arr);
-PrintArrya(arr);
-Console.WriteLine();
-// FillArrya1(arr);
-// PrintArrya(arr);
-Console.WriteLine();
+PrintArray(FillArray(array));
