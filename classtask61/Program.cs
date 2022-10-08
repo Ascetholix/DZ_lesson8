9ab55a41d6b430c899f155471a01f074f8419526
Ascetholix <sangak@mail.ru>
2022-10-08 11:40:16 +0300

    Add pascal

--- classtask61/Program.cs
+++ classtask61/Program.cs
@@ -1,2 +1,67 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+// void PrintArray(int[,] arg)
+// {
+//   for (int i = 0; i < arg.GetLength(0); i++)
+//   {
+//     for (int j = 0; j < arg.GetLength(1); j++)
+//     {
+//       Console.Write($"{arg[i, j]} ");
+//     }
+//     Console.WriteLine();
+//   }
+// }
+
+void FillArray(int[,] arg)
+{
+  for (int i = 0; i < arg.GetLength(0); i++)
+  {
+    for (int j = 0; j < arg.GetLength(1); j++)
+    {
+      if (i == 0) arg[i, j] = 1;
+      if (j == i) arg[i, j] = 1;
+      if (j > 0 && j < i) arg[i, j] = arg[i - 1, j - 1] + arg[i - 1, j]; 
+    }
+  }
+}
+
+
+string[,] ArrayPas(int[,] arg)
+{
+  string[,] arg1 = new string[arg.GetLength(0), arg.GetLength(1)];
+
+  for (int i = 0; i < arg.GetLength(0); i++)
+  {
+    for (int j = 0; j < arg.GetLength(1); j++)
+    {
+      if (i != 0 && j != 0)
+      {
+        arg1[i, j] = Convert.ToString(arg[i, j]);
+      }
+    }
+  }
+  return arg1;
+}
+void PrintArrayPas(string[,] arg)
+{
+  for (int i = 0; i < arg.GetLength(0); i++)
+  {
+    for (int j = 0; j < arg.GetLength(1); j++)
+    {
+      if (arg[i, j] == "0") Console.Write($" ");
+      else Console.Write($"\t{arg[i, j]} ");
+    }
+    Console.WriteLine();
+  }
+}
+
+Console.Write("Введите длину строки: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите длину столбца: ");
+int cols = Convert.ToInt32(Console.ReadLine());
+
+int[,] arr = new int[rows, cols];
+FillArray(arr);
+string[,] a = ArrayPas(arr);
+
+PrintArrayPas(a);
--- task62/Program.cs
+++ task62/Program.cs
@@ -8,52 +8,62 @@ Console.Clear();
 
 void PrintArrya(int[,] arg)   // Метод вывода матрицы
 {
-    for (int i = 0; i < arg.GetLength(0); i++) // rows - строки
+  for (int i = 0; i < arg.GetLength(0); i++) // rows - строки
+  {
+    for (int j = 0; j < arg.GetLength(1); j++)  // columns - столбцы
     {
-        for (int j = 0; j < arg.GetLength(1); j++)  // columns - столбцы
-        {
-            Console.Write($" {arg[i, j]} ");
-        }
-        Console.WriteLine();
+      Console.Write($" {arg[i, j]} ");
     }
+    Console.WriteLine();
+  }
 }
 
-void FillArrya(int[,] arg)
+int[,] FillArrya(int[,] arg)
 {
-    int[] mas = new int[arg.GetLength(0) * arg.GetLength(1)];
-    int cout = 0;
-    for (int i = 0; i < arg.GetLength(0); i++)
+  int[] mas = new int[arg.GetLength(0) * arg.GetLength(1)];
+  int N = arg.GetLength(0);
+  int L = arg.GetLength(1);
+  int i = 0;
+  int j = 0;
+  for (int h = 1; h < N + L; h++)
+  {
+
+    int a = 0;
+    if (i == 0 && j < N)
     {
-        // bool ch = true;
-
-        for (int j = 0; j < arg.GetLength(1); j++)
-        {
-
-            arg[i, j] = mas[cout];
-            cout++;
-            for (int k = 0; k < mas.Length; k++)
-            {
-
-                mas[cout] = k + 1;
-                break;
-
-            }
-
-        }
+      arg[i, j] = h;
+      j++;
     }
+    if (j + 1 == L && i < L)
+    {
+      a = h + 1;
+      arg[i, j] = a;
+      i++;
+    }
+  }
+//   int l = (N - 1) - 1;
+//   int k = (L - 1) - 1;
+
+//   for (int h = 0; h < (N - 1) + (L - 1); h++)
+//   {
+
+//     if (l == N - 1 && k < N )
+//     {
+//       arg[l, k] = h;
+//       k++;
+//     }
+
+//   }
+  return arg;
 }
 
-Console.Write("Введите длину строки ");
-int row = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите длину строки ");
+// int row = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Введите длину столбца ");
-int colums = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите длину столбца ");
+// int colums = Convert.ToInt32(Console.ReadLine());
 
-int[,] arr = new int[row, colums];
+int[,] arr = new int[4, 4];
 
-FillArrya(arr);
-PrintArrya(arr);
-Console.WriteLine();
-// FillArrya1(arr);
-// PrintArrya(arr);
-Console.WriteLine();
+PrintArrya(FillArrya(arr));
+
